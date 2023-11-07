9ec7b6d4cfa04348b05611f4bdb8ff8e8b3d6ae2
tamanna986 <tamanna.986jahan@gmail.com>
2023-11-08 00:33:22 +0600

    jwt token added and veryfy token added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -9,17 +11,42 @@ require('dotenv').config();
 
 
 // middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin:[
+          'http://localhost:5173'
+          
+        ],
+        credentials: true
+      }
+));
 app.use(express.json());
+app.use(cookieParser());
 
 
-app.get('/', (req,res) =>{
-    res.send('study buddy server is running')
-})
+// selfmade middleware
+const verifyToken = async(req,res,next) =>{
+    const token = req.cookies?.token;
+    if(!token){
+      return res.status(401).send({message:'unAuthorized'})
+    }
+  
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRETE, (err,decoded) => {
+      if(err){
+        return  res.status(401).send({message:'unAuthorized'})
+      }
+  
+      console.log('value in token', decoded)
+     
+      req.user = decoded;
+      next();
+  
+    })
+  
+  }
 
-app.listen(port, ()=>{
-    console.log(`server is running on ${port}`)
-})
+
+
 
 
 
@@ -44,7 +71,35 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
+
+
+    //  to generate token using jwt FOR SIGN IN 
+  app.post('/jwt',verifyToken, async(req,res) =>{
+    const user = req.body;
+    console.log(user);
+    const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRETE,{expiresIn:'1h'});
+    console.log(token)
+    res
+    .cookie('token', token, {
+      httpOnly: true,
+      // secure: false,
+    //   sameSite: 'none'
+    })
+    .send({success:true})
+    console.log(user)
+  })
+
+
+  // TO CHECK WHICH USER LOGGED OUT
+
+  app.post('/logout', async(req, res) =>{
+    const user = req.body;
+    console.log('LOGGESD OUT USER')
+
+    // to clear logged users cookie while logging out
+     res.clearCookie('token', {maxAge:0}).send({success:true})
+})
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -52,3 +107,13 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+
+
+app.get('/', (req,res) =>{
+    res.send('study buddy server is running')
+})
+
+app.listen(port, ()=>{
+    console.log(`server is running on ${port}`)
+})
