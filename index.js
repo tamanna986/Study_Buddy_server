686bd55f74b6c0ec97d990fccd7a34353eafff97
tamanna986 <tamanna.986jahan@gmail.com>
2023-11-08 19:05:18 +0600

    category based data selected in all assignment page

--- index.js
+++ index.js
@@ -12,38 +12,38 @@ require('dotenv').config();
 
 // middleware
 app.use(cors(
-    {
-        origin:[
-          'http://localhost:5173'
-          
-        ],
-        credentials: true
-      }
+  {
+    origin: [
+      'http://localhost:5173'
+
+    ],
+    credentials: true
+  }
 ));
 app.use(express.json());
 app.use(cookieParser());
 
 
 // selfmade middleware
-const verifyToken = async(req,res,next) =>{
-    const token = req.cookies?.token;
-    if(!token){
-      return res.status(401).send({message:'unAuthorized'})
-    }
-  
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRETE, (err,decoded) => {
-      if(err){
-        return  res.status(401).send({message:'unAuthorized'})
-      }
-  
-      console.log('value in token', decoded)
-     
-      req.user = decoded;
-      next();
-  
-    })
-  
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: 'unAuthorized' })
   }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRETE, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'unAuthorized' })
+    }
+
+    console.log('value in token', decoded)
+
+    req.user = decoded;
+    next();
+
+  })
+
+}
 
 
 
@@ -70,36 +70,68 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+    const db = client.db('studyBuddy');
+    const assignmentCollection = client.db('studyBuddy').collection('assignments')
+
+    // for adding assignments
+
+    app.post('/assignments', async (req, res) => {
+      const newAssignment = req.body;
+      console.log(newAssignment)
+      const result = await assignmentCollection.insertOne(newAssignment)
+      res.send(result);
+    })
+
+
+    // to get all assignments in all assignments page
+    app.get('/allAssignments', async (req, res) => {
+      const cursor = assignmentCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    // to get assignments by category
+    app.get('/assignmentsByCategory', async (req, res) => {
+      const category = req.query.category;
+      const cursor = assignmentCollection.find({ category: category });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
 
 
     //  to generate token using jwt FOR SIGN IN 
-  app.post('/jwt',verifyToken, async(req,res) =>{
-    const user = req.body;
-    console.log(user);
-    const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRETE,{expiresIn:'1h'});
-    console.log(token)
-    res
-    .cookie('token', token, {
-      httpOnly: true,
-      // secure: false,
-    //   sameSite: 'none'
+    app.post('/jwt', verifyToken, async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRETE, { expiresIn: '1h' });
+      console.log(token)
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          // secure: false,
+          //   sameSite: 'none'
+        })
+        .send({ success: true })
+      console.log(user)
     })
-    .send({success:true})
-    console.log(user)
-  })
 
 
-  // TO CHECK WHICH USER LOGGED OUT
+    // TO CHECK WHICH USER LOGGED OUT
 
-  app.post('/logout', async(req, res) =>{
-    const user = req.body;
-    console.log('LOGGESD OUT USER')
+    app.post('/logout', async (req, res) => {
+      const user = req.body;
+      console.log('LOGGESD OUT USER')
 
-    // to clear logged users cookie while logging out
-     res.clearCookie('token', {maxAge:0}).send({success:true})
-})
+      // to clear logged users cookie while logging out
+      res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+    })
+
+
+
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -110,10 +142,10 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req,res) =>{
-    res.send('study buddy server is running')
+app.get('/', (req, res) => {
+  res.send('study buddy server is running')
 })
 
-app.listen(port, ()=>{
-    console.log(`server is running on ${port}`)
+app.listen(port, () => {
+  console.log(`server is running on ${port}`)
 })
