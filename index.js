83ecea2ac76739f06263b95cccfe3c429a82bb8b
tamanna986 <tamanna.986jahan@gmail.com>
2023-11-09 14:42:44 +0600

    delete oparation added in all assignment pages

--- index.js
+++ index.js
@@ -185,6 +185,15 @@ app.put('/update/:id', async(req,res) =>{
 })
 
 
+// to delete data
+app.delete('/allAssignments/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await assignmentCollection.deleteOne(query);
+  res.send(result);
+});
+
+
 
 
 
